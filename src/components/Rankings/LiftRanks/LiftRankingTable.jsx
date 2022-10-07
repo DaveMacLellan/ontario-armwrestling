fc0f478edbff56076065a2efdcceef97d10ded31
DaveMacLellan <davemaclellanx@gmail.com>
2022-10-07 03:09:35 -0400

    Finished top 10
    added ranks 4-10.
    Fixed layout of ranks on mobile.

--- src/components/Rankings/LiftRanks/LiftRankingTable.jsx
+++ src/components/Rankings/LiftRanks/LiftRankingTable.jsx
@@ -1,10 +1,10 @@
 
 export default function LiftRankingTable(props) {
-    //console.log(props.test[0].name)
+    console.log(props.test[3].name)
     return(
         <>
             <table className="table-ranks" >
-                <caption className="table-caption" >{props.title}</caption>
+                <caption className="table-caption-lift" >{props.title}</caption>
                 <thead>
                     <tr>
                         <th>Rank</th>
@@ -28,7 +28,41 @@ export default function LiftRankingTable(props) {
                         <td>{props.test[2].name}</td>
                         <td>{props.test[2].weight}</td>
                     </tr>
-                    
+                    <tr>
+                        <td className="rank-number">4</td>
+                        <td>{props.test[3].name}</td>
+                        <td>{props.test[3].weight}</td>
+                    </tr>
+                    <tr>
+                        <td className="rank-number">5</td>
+                        <td>{props.test[4].name}</td>
+                        <td>{props.test[4].weight}</td>
+                    </tr>
+                    <tr>
+                        <td className="rank-number">6</td>
+                        <td>{props.test[5].name}</td>
+                        <td>{props.test[5].weight}</td>
+                    </tr>
+                    <tr>
+                        <td className="rank-number">7</td>
+                        <td>{props.test[6].name}</td>
+                        <td>{props.test[6].weight}</td>
+                    </tr>
+                    <tr>
+                        <td className="rank-number">8</td>
+                        <td>{props.test[7].name}</td>
+                        <td>{props.test[7].weight}</td>
+                    </tr>
+                    <tr>
+                        <td className="rank-number">9</td>
+                        <td>{props.test[8].name}</td>
+                        <td>{props.test[8].weight}</td>
+                    </tr>
+                    <tr>
+                        <td className="rank-number">10</td>
+                        <td>{props.test[9].name}</td>
+                        <td>{props.test[9].weight}</td>
+                    </tr>
                     
                 </tbody>
             </table>
